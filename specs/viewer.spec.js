ac0220d81f257e23185068b532a37ca6f732fbaf
Phillip Green <phillip.green@idmworks.com>
2012-08-21 17:07:17 -0400

    viewer with specs

---
+++ specs/viewer.spec.js
@@ -0,0 +1,280 @@
+"use strict";
+
+var viewer = require('../viewer.js'), util = require('util');
+
+describe('viewer', function() {
+
+  describe('addArtifactTo', function() {
+    var addArtifactTo = viewer._internals.addArtifactTo;
+
+    describe('releases', function() {
+
+      it('add jar for release', function() {
+        var artifactInfo = {
+          groupId : 'com.ii.green.phillip.nodeMavenRepo',
+          artifactId : 'noddeMavenRepo-test-project',
+          version : '2.0',
+          artifacts : []
+        };
+        var url = '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0.jar';
+        addArtifactTo(artifactInfo, url);
+
+        expect(artifactInfo.artifacts.length).toEqual(1);
+        expect(artifactInfo.artifacts[0]).toEqual({
+          extension : 'jar',
+          classifier : '',
+          url : url,
+          checksumUrls : {
+          }
+        });
+      });
+
+      it('add md5 checksum of jar for release', function() {
+        var artifactInfo = {
+          groupId : 'com.ii.green.phillip.nodeMavenRepo',
+          artifactId : 'noddeMavenRepo-test-project',
+          version : '2.0',
+          artifacts : [{
+            extension : 'jar',
+            classifier : '',
+            url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0.jar',
+            checksumUrls : {
+            }
+          }]
+        };
+        var url = '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0.jar.md5';
+        addArtifactTo(artifactInfo, url);
+
+        expect(artifactInfo.artifacts.length).toEqual(1);
+        expect(artifactInfo.artifacts[0]).toEqual({
+          extension : 'jar',
+          classifier : '',
+          url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0.jar',
+          checksumUrls : {
+            md5 : url
+          }
+        });
+      });
+
+      it('add sources jar for release', function() {
+        var artifactInfo = {
+          groupId : 'com.ii.green.phillip.nodeMavenRepo',
+          artifactId : 'noddeMavenRepo-test-project',
+          version : '2.0',
+          artifacts : [{
+            extension : 'jar',
+            classifier : '',
+            url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0.jar',
+            checksumUrls : {
+              md5 : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0.jar.md5'
+            }
+          }]
+        };
+        var url = '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-sources.jar';
+        addArtifactTo(artifactInfo, url);
+
+        expect(artifactInfo.artifacts.length).toEqual(2);
+        expect(artifactInfo.artifacts[1]).toEqual({
+          extension : 'jar',
+          classifier : 'sources',
+          url : url,
+          checksumUrls : {
+          }
+        });
+      });
+
+      it('add md5 checksum of sources jar for release', function() {
+        var artifactInfo = {
+          groupId : 'com.ii.green.phillip.nodeMavenRepo',
+          artifactId : 'noddeMavenRepo-test-project',
+          version : '2.0',
+          artifacts : [{
+            extension : 'jar',
+            classifier : '',
+            url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0.jar',
+            checksumUrls : {
+              md5 : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0.jar.md5'
+            }
+          }, {
+            extension : 'jar',
+            classifier : 'sources',
+            url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-sources.jar',
+            checksumUrls : {
+            }
+          }]
+        };
+        var url = '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-sources.jar.md5';
+
+        addArtifactTo(artifactInfo, url);
+
+        expect(artifactInfo.artifacts.length).toEqual(2);
+        expect(artifactInfo.artifacts[1]).toEqual({
+          extension : 'jar',
+          classifier : 'sources',
+          url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-sources.jar',
+          checksumUrls : {
+            md5 : url
+          }
+        });
+      });
+
+    });
+
+    describe('snapshots', function() {
+
+      it('add jar for snapshot', function() {
+        var artifactInfo = {
+          groupId : 'com.ii.green.phillip.nodeMavenRepo',
+          artifactId : 'noddeMavenRepo-test-project',
+          version : '2.0-SNAPSHOT',
+          snapshot : true,
+          timestamp : '20120821.120944',
+          buildNumber : '1',
+          artifacts : []
+        };
+        var url = '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0-SNAPSHOT/noddeMavenRepo-test-project-2.0-20120821.120944-1.jar';
+        addArtifactTo(artifactInfo, url);
+
+        expect(artifactInfo.artifacts.length).toEqual(1);
+        expect(artifactInfo.artifacts[0]).toEqual({
+          extension : 'jar',
+          classifier : '',
+          url : url,
+          checksumUrls : {
+          }
+        });
+      });
+
+      it('add md5 checksum of jar for snapshot', function() {
+        var artifactInfo = {
+          groupId : 'com.ii.green.phillip.nodeMavenRepo',
+          artifactId : 'noddeMavenRepo-test-project',
+          version : '2.0-SNAPSHOT',
+          snapshot : true,
+          timestamp : '20120821.120944',
+          buildNumber : '1',
+          artifacts : [{
+            extension : 'jar',
+            classifier : '',
+            url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0-SNAPSHOT/noddeMavenRepo-test-project-2.0-20120821.120944-1.jar',
+            checksumUrls : {
+            }
+          }]
+        };
+        var url = '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0-SNAPSHOT/noddeMavenRepo-test-project-2.0-20120821.120944-1.jar.md5';
+        addArtifactTo(artifactInfo, url);
+
+        expect(artifactInfo.artifacts.length).toEqual(1);
+        expect(artifactInfo.artifacts[0]).toEqual({
+          extension : 'jar',
+          classifier : '',
+          url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0-SNAPSHOT/noddeMavenRepo-test-project-2.0-20120821.120944-1.jar',
+          checksumUrls : {
+            md5 : url
+          }
+        });
+      });
+
+      it('add sources jar for snapshot', function() {
+        var artifactInfo = {
+          groupId : 'com.ii.green.phillip.nodeMavenRepo',
+          artifactId : 'noddeMavenRepo-test-project',
+          version : '2.0-SNAPSHOT',
+          snapshot : true,
+          timestamp : '20120821.120944',
+          buildNumber : '1',
+          artifacts : [{
+            extension : 'jar',
+            classifier : '',
+            url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0-SNAPSHOT/noddeMavenRepo-test-project-2.0-20120821.120944-1.jar',
+            checksumUrls : {
+              md5 : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0-SNAPSHOT/noddeMavenRepo-test-project-2.0-20120821.120944-1.jar.md5'
+            }
+          }]
+        };
+        var url = '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-20120821.120944-1-sources.jar';
+        addArtifactTo(artifactInfo, url);
+
+        expect(artifactInfo.artifacts.length).toEqual(2);
+        expect(artifactInfo.artifacts[1]).toEqual({
+          extension : 'jar',
+          classifier : 'sources',
+          url : url,
+          checksumUrls : {
+          }
+        });
+      });
+
+      it('add md5 checksum of sources jar for snapshot', function() {
+        var artifactInfo = {
+          groupId : 'com.ii.green.phillip.nodeMavenRepo',
+          artifactId : 'noddeMavenRepo-test-project',
+          version : '2.0-SNAPSHOT',
+          snapshot : true,
+          timestamp : '20120821.120944',
+          buildNumber : '1',
+          artifacts : [{
+            extension : 'jar',
+            classifier : '',
+            url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-20120821.120944-1.jar',
+            checksumUrls : {
+              md5 : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-20120821.120944-1.jar.md5'
+            }
+          }, {
+            extension : 'jar',
+            classifier : 'sources',
+            url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-20120821.120944-1-sources.jar',
+            checksumUrls : {
+            }
+          }]
+        };
+        var url = '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-20120821.120944-1-sources.jar.md5';
+        addArtifactTo(artifactInfo, url);
+
+        expect(artifactInfo.artifacts.length).toEqual(2);
+        expect(artifactInfo.artifacts[1]).toEqual({
+          extension : 'jar',
+          classifier : 'sources',
+          url : '/com/ii/green/phillip/nodeMavenRepo/noddeMavenRepo-test-project/2.0/noddeMavenRepo-test-project-2.0-20120821.120944-1-sources.jar',
+          checksumUrls : {
+            md5 : url
+          }
+        });
+      });
+
+    });
+  });
+
+  describe('crawlPathForFilesPromise', function() {
+    it('crawl repo for files', function(done) {
+      viewer._internals.crawlPathForFilesPromise('specs/mock-repo').then(function(files) {
+        expect(files.length).toEqual(96);
+        done();
+      });
+    });
+  });
+
+  describe('buildStructureFromFilesPromise', function() {
+    it('build structure from files', function(done) {
+      var rootPath = '';
+      var files = [];
+      viewer._internals.buildStructureFromFilesPromise(rootPath, files).then(function(structure) {
+        console.log("structure", util.inspect(structure, false, null));
+        expect(structure).toBeDefined();
+        done();
+      });
+    });
+  });
+
+  describe('buildStructure', function() {
+    it('build structure from repo', function(done) {
+      viewer.buildStructure('specs/mock-repo', function(err, structure) {
+        expect(err).not.toBeDefined();
+        expect(structure).toBeDefined();
+        console.log("structure", util.inspect(structure, false, null));
+        done();
+      });
+    });
+  });
+
+});
--- viewer.js
+++ viewer.js
@@ -2,100 +2,248 @@
 
 exports = module.exports;
 
-var fs = require('fs'), http = require('http'), util = require('util'), crypto = require('crypto'), xml2js = require('xml2js');
+var fs = require('fs'), util = require('util'), xml2js = require('xml2js'), q = require('q');
 var xmlParser = new xml2js.Parser();
 
-var StructureBuilder = function(repoPath) {
-  var self = this;
-  var MAVEN_METADATA_FILENAME = 'maven-metadata.xml';
+var MAVEN_METADATA_FILENAME = 'maven-metadata.xml';
+var SNAPSHOT = 'SNAPSHOT';
 
-  self.structure = [];
+var readdirPromise = q.nbind(fs.readdir, fs);
+var statPromise = q.nbind(fs.stat, fs);
 
-  function handleMavenMetadata(file, callback) {
+var readFilePromise = q.nbind(fs.readFile, fs);
+var parseXmlPromise = q.nbind(xmlParser.parseString, xmlParser);
 
-    fs.readFile(file, function(err, data) {
-      xmlParser.parseString(data, function(err, result) {
-        self.structure.push({
-          file : file,
-          data : result
+function crawlPathForFilesPromise(path) {
+  return statPromise(path).then(function(stats) {
+    if (stats.isDirectory()) {
+      return readdirPromise(path).then(function(files) {
+        var childPromises = [];
+        files.forEach(function(childpath) {
+          var fullChildPath = path + "/" + childpath;
+          childPromises.push(crawlPathForFilesPromise(fullChildPath));
         });
-        //console.dir(result);
-        callback();
-      });
-    });
+        return q.allResolved(childPromises).then(function(completedChildPromises) {
+          var artifactFiles = [];
+          completedChildPromises.forEach(function(childPromise) {
+            var value = childPromise.valueOf();
 
-  }
-
-  function handleFile(file, callback) {
-    if (file.substr(-1 * MAVEN_METADATA_FILENAME.length) === MAVEN_METADATA_FILENAME) {
-      handleMavenMetadata(file, callback);
-    } else {
-      callback();
-    }
-  }
-
-  function contains(array, value) {
-    var len = array.length;
-    for (var i = 0; i < len; i++) {
-      if (len[i] === value) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  function handleArtifact(directory, files, callback) {
-
-  }
-
-  function crawlDirectoryTree(root, callback) {
-    fs.stat(root, function(err, stats) {
-      if (err) {
-        _handleError(err);
-      } else if (!stats.isDirectory()) {
-        if (stats.isFile()) {
-          handleFile(root, callback);
-        } else {
-          _handleError('Unsupported File Type:', stats);
-        }
-      } else {
-        fs.readdir(root, function(err, files) {
-          if (err) {
-            _handleError(err);
-          } else {
-            if (contains(files, MAVEN_METADATA_FILENAME)) {
-              handleArtifact(root, files, callback);
-            } else {
-              var nextCallBack = callback;
-              for (var i = files.length - 1; i >= 0; i--) {
-                nextCallBack = (function(newRoot, nextCallBack) {
-                  return function() {
-                    crawlDirectoryTree(newRoot, nextCallBack)
-                  };
-                })(root + "/" + files[i], nextCallBack);
+            if (value) {
+              if (util.isArray(value)) {
+                artifactFiles.push.apply(artifactFiles, value);
+              } else {
+                artifactFiles.push(value);
               }
-              nextCallBack();
             }
-          }
+          });
+          return q.resolve(artifactFiles);
         });
-      }
-    });
-  }
+      });
+    } else if (stats.isFile()) {
+      return path;
+    } else {
+      return q.fcall(function() {
+        throw new Error("Unsupported file type: " + stats);
+      });
+    }
+  });
+}
 
+function endsWith(str, suffix) {
+  return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
 
-  self.buildStructure = function(callback) {
-    crawlDirectoryTree(repoPath, function() {
-      callback(self.err, self.structure);
-    });
-  }
-  var _handleError = function(err) {
-    console.error.apply(arguments);
-    self.err = err;
+function contains(str, substr) {
+  return str.indexOf(substr) !== -1;
+}
+
+function startsWith(str, prefix) {
+  return str.indexOf(prefix) === 0;
+}
+
+function asList(val) {
+  if (util.isArray(val)) {
+    return val;
+  } else {
+    return [val];
   }
 }
-var buildStructure = function(repoPath, callback) {
-  var sb = new StructureBuilder(repoPath);
-  sb.buildStructure(callback);
+
+function addArtifactTo(artifactInfo, url, snapshotId) {
+
+  var fileName = url.split('/').slice(-1)[0];
+  var prefix;
+  if (artifactInfo.snapshot) {
+    prefix = [artifactInfo.artifactId, artifactInfo.version.replace('-SNAPSHOT', ''), artifactInfo.timestamp, artifactInfo.buildNumber].join('-');
+  } else {
+    prefix = [artifactInfo.artifactId, artifactInfo.version].join('-');
+  }
+
+  if (fileName.indexOf(prefix) !== 0) {
+    console.error('incorrect fileName', prefix, fileName);
+    return;
+  }
+
+  var remainder = fileName.replace(prefix, '');
+  var parts = remainder.split('.');
+
+  var classifier = (parts[0] || '').replace('-', '');
+  var extension = parts[1];
+  var checksumType = parts.length === 3 ? parts[2] : null;
+
+  var matches = artifactInfo.artifacts.filter(function(artifact) {
+    return artifact.classifier === classifier && artifact.extension === extension;
+  });
+
+  var artifact;
+  if (matches.length === 0) {
+    artifact = {
+      extension : extension,
+      classifier : classifier,
+      url : '',
+      checksumUrls : {
+      }
+    }
+    artifactInfo.artifacts.push(artifact);
+  } else {
+    artifact = matches[0];
+  }
+
+  if (!checksumType) {
+    artifact.url = url;
+  } else {
+    artifact.checksumUrls[checksumType] = url;
+  }
+}
+
+function buildStructureFromFilesPromise(rootPath, files) {
+  //sort so that artifact level metadata files appear before version level metadata files and
+  //original files appear before checksum
+  files.sort(function(a, b) {
+    return a.length - b.length;
+  });
+
+  // strip repo prefix to create urls
+  var urls = files.map(function(file) {
+    return file.replace(rootPath, '');
+  });
+
+  //filter metadata files
+  var metadataFiles = files.filter(function(file) {
+    return endsWith(file, MAVEN_METADATA_FILENAME);
+  });
+
+  var childPromises = [];
+  metadataFiles.forEach(function(metadataFile) {
+    childPromises.push(readFilePromise(metadataFile).then(parseXmlPromise));
+  });
+  return q.allResolved(childPromises).then(function(completedChildPromises) {
+    var structure = {};
+    completedChildPromises.forEach(function(childPromise) {
+      var metadata = childPromise.valueOf();
+
+      if (metadata) {
+        if (!structure.hasOwnProperty(metadata.groupId)) {
+          structure[metadata.groupId] = {};
+        }
+        var group = structure[metadata.groupId];
+        if (!group.hasOwnProperty(metadata.artifactId)) {
+          group[metadata.artifactId] = {};
+        }
+        var artifact = group[metadata.artifactId];
+        if (!metadata.hasOwnProperty('version')) {
+          asList(metadata.versioning.versions.version).forEach(function(version) {
+            if (!artifact.hasOwnProperty(version)) {
+              artifact[version] = {
+                groupId : metadata.groupId,
+                artifactId : metadata.artifactId,
+                version : version,
+                artifacts : []
+              };
+            }
+
+            //only handle releases
+            if (!endsWith(version, SNAPSHOT)) {
+              artifact[version].snapshot = false;
+
+              var urlPrefix = '/' + metadata.groupId.replace(new RegExp("\\.", "gm"), '/') + '/' + metadata.artifactId + '/' + version + '/';
+
+              var artifactVersionUrls = urls.filter(function(url) {
+                return startsWith(url, urlPrefix) && !contains(url, MAVEN_METADATA_FILENAME);
+              });
+              artifactVersionUrls.forEach(function(url) {
+                addArtifactTo(artifact[version], url);
+              });
+            } else {
+              artifact[version].snapshot = true;
+            }
+          });
+        } else {
+          var version = metadata.version;
+          if (!artifact.hasOwnProperty(version)) {
+            artifact[version] = {
+              groupId : metadata.groupId,
+              artifactId : metadata.artifactId,
+              version : version,
+              snapshot : true,
+              artifacts : []
+            };
+          }
+          artifact[version].timestamp = metadata.versioning.snapshot.timestamp;
+          artifact[version].buildNumber = metadata.versioning.snapshot.buildNumber;
+
+          var urlPrefix = '/' + metadata.groupId.replace(new RegExp("\\.", "gm"), '/') + '/' + metadata.artifactId + '/' + version + '/';
+
+          var snapshotId = artifact[version].timestamp + '-' + artifact[version].buildNumber;
+          var artifactVersionUrls = urls.filter(function(url) {
+            return startsWith(url, urlPrefix) && !contains(url, MAVEN_METADATA_FILENAME) && contains(url, snapshotId);
+          });
+          artifactVersionUrls.forEach(function(url) {
+            addArtifactTo(artifact[version], url);
+          });
+        }
+      }
+    });
+    return structure;
+  });
+
+  var example = {
+    groupId : '',
+    artifactId : '',
+    version : '',
+    artifacts : [{
+      extension : '',
+      classifier : '',
+      url : '',
+      checksumUrls : {
+        sha1 : '',
+        md5 : ''
+      }
+    }]
+  };
+
+  //return q.resolve(structure);
+}
+
+var _handleError = function(err) {
+  console.error.apply(console, arguments);
+}
+var buildStructure = function(path, callback) {
+  console.log('building structure from', path);
+  crawlPathForFilesPromise(path).then(function(files) {
+    return buildStructureFromFilesPromise(path, files)
+  }).then(function(structure) {
+    callback((
+    void 0), structure);
+  }).fail(function(err) {
+    _handleError(err);
+    callback(err);
+  }).end();
 }
 
 exports.buildStructure = buildStructure;
+exports._internals = {};
+exports._internals.crawlPathForFilesPromise = crawlPathForFilesPromise;
+exports._internals.buildStructureFromFilesPromise = buildStructureFromFilesPromise;
+exports._internals.addArtifactTo = addArtifactTo;
+
